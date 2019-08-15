cf85cc2d1d9e07931d6540b3403333f7335cc65d
haykIsayan <haykisayan@gmail.com>
2019-08-15 09:23:42 +0400

    Created a custom view for handling Authentication. Established a navigation graph.

--- app/src/main/java/com/example/project_auth/view/MainActivity.java
+++ app/src/main/java/com/example/project_auth/view/MainActivity.java
@@ -1,42 +1,19 @@
 package com.example.project_auth.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
-
 import com.example.project_auth.R;
-import com.example.project_auth.model.User;
-import com.example.project_auth.presentation.LoginPresenter;
-import com.example.project_auth.presentation.LoginViewController;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-public class MainActivity extends AppCompatActivity implements LoginViewController {
-
-    private LoginPresenter mLoginPresenter;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLoginPresenter = new LoginPresenter(this, this.getApplication());
-        mLoginPresenter.init();
-    }
-
-    @Override
-    public void onLoginSuccess(@NotNull User user) {
-
-    }
-
-    @Override
-    public void showLoading(@NotNull String message) {
-
-    }
-
-    @Override
-    public void onLoginFailed(@Nullable Throwable throwable) {
-
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+                .navigate(R.id.destination_login);
     }
 }
